76cdb350d393c5c6e7ae67d1b6dad869b1bb11e8
Sacha Weatherstone <sachaw100@hotmail.com>
2020-12-26 23:53:11 +1030

    export protobuf types (#26)

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,13 @@ import { Client } from "./client";
 import { IBLEConnection } from "./ibleconnection";
 import { IHTTPConnection } from "./ihttpconnection";
 import { NodeDB } from "./nodedb";
+import * as Protobuf from "./protobuf";
 
-export { SettingsManager, Client, IBLEConnection, IHTTPConnection, NodeDB };
+export {
+  SettingsManager,
+  Client,
+  IBLEConnection,
+  IHTTPConnection,
+  NodeDB,
+  Protobuf,
+};
