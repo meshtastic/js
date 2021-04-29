ea2f16058c3dd2ad61e2fd82c4211ee946b0d5d5
Sacha Weatherstone <sachaw100@hotmail.com>
2021-04-29 17:10:02 +1000

    Implement serial connections

--- src/iserialconnection.ts
+++ src/iserialconnection.ts
@@ -3,30 +3,98 @@ import { IMeshDevice } from "./imeshdevice";
 import type { serialConnectionParameters } from "./types";
 
 /**
- * Allows to connect to a meshtastic device over HTTP(S)
- * @description NOT YET IMPLEMENTED
+ * Allows to connect to a meshtastic device over WebSerial
  */
 export class ISerialConnection extends IMeshDevice {
+  /**
+   *
+   */
   private port: SerialPort | undefined;
+
+  /**
+   *
+   */
+  private reader: ReadableStreamDefaultReader<Uint8Array>;
+
+  /**
+   *
+   */
+  private writer: WritableStream<ArrayBuffer>;
 
   constructor() {
     super();
 
     this.port = undefined;
+
+    this.reader = new ReadableStreamDefaultReader(new ReadableStream());
+    this.writer = new WritableStream();
   }
 
-  /**
-   */
+  private async readLoop() {
+    while (true) {
+      const { value, done } = await this.reader.read();
+      if (value) {
+        this.handleFromRadio(value);
+      }
+
+      if (done) {
+        this.reader.releaseLock();
+        break;
+      }
+    }
+  }
+
   public async connect(parameters: serialConnectionParameters): Promise<void> {
     this.port = await navigator.serial.requestPort();
+
     await this.port.open({
       baudRate: parameters.baudRate ? parameters.baudRate : 921600
     });
+
+    let byteBuffer = new Uint8Array([]);
+
+    if (this.port.readable && this.port.writable) {
+      this.reader = this.port.readable
+        .pipeThrough(
+          new TransformStream({
+            start(controller) {
+              controller.enqueue(byteBuffer);
+            },
+            transform(chunk: Uint8Array, controller) {
+              byteBuffer = new Uint8Array([...byteBuffer, ...chunk]);
+
+              if (byteBuffer.includes(0x94)) {
+                const index = byteBuffer.findIndex((byte) => byte === 0x94);
+                const startBit2 = byteBuffer[index + 1];
+                const msb = byteBuffer[index + 2];
+                const lsb = byteBuffer[index + 3];
+
+                if (
+                  startBit2 === 0xc3 &&
+                  byteBuffer.length >= index + 4 + lsb + msb
+                ) {
+                  controller.enqueue(
+                    byteBuffer.subarray(index + 4, index + 4 + lsb + msb)
+                  );
+                  byteBuffer = byteBuffer.slice(index + 4 + lsb + msb);
+                }
+              }
+            },
+            flush(transformer) {
+              transformer.enqueue(byteBuffer);
+            }
+          })
+        )
+        .getReader();
+
+      this.writer = this.port.writable;
+    }
+
+    this.readLoop();
+
+    await this.configure();
   }
 
-  /**
-   * Disconnects from the meshtastic device
-   */
   public disconnect(): void {
     this.onDeviceStatusEvent.next(Types.DeviceStatusEnum.DEVICE_DISCONNECTED);
   }
@@ -46,5 +114,9 @@ export class ISerialConnection extends IMeshDevice {
   /**
    * Short description
    */
-  protected async writeToRadio(data: Uint8Array): Promise<void> {}
+  protected async writeToRadio(data: Uint8Array): Promise<void> {
+    const writer = this.writer.getWriter();
+    writer.write(new Uint8Array([0x94, 0xc3, 0x00, data.length, ...data]));
+    writer.releaseLock();
+  }
 }
--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,28 +1,5 @@
-import { LogRecord_Level } from "./generated/mesh";
-import { SettingsManager } from "./settingsmanager";
-
-/**
- * Converts a `ArrayBuffer` to a hex string
- * @param arrayBuffer Input `ArrayBuffer` to be converted
- */
-export const bufferToHex = (arrayBuffer: ArrayBuffer): string => {
-  return Array.prototype.map
-    .call(new Uint8Array(arrayBuffer), (x: number) =>
-      `00${x.toString(16)}`.slice(-2)
-    )
-    .join("") as string;
-};
-
-/**
- * Converts a `Uint8Array` to an `ArrayBuffer`
- * @param array Input `Uint8Array` to be converted
- */
-export const typedArrayToBuffer = (array: Uint8Array): ArrayBuffer => {
-  return array.buffer.slice(
-    array.byteOffset,
-    array.byteLength + array.byteOffset
-  );
-};
+import { LogRecord_Level } from "../generated";
+import { SettingsManager } from "../settingsmanager";
 
 /**
  * Global event logger
