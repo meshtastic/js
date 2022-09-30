eddc98efa2646ac913d405b97b0e4182704896b7
Sacha Weatherstone <sachaw100@hotmail.com>
2022-09-30 12:13:00 +0930

    New ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,25 @@
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: "@meshtastic/eslint-config",
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict",
+    "plugin:jsdoc/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"]
+  },
+  plugins: ["@typescript-eslint", "import", "jsdoc"],
+  root: true,
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true
+    }
   }
 };
