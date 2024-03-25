07cae5de246c76b10a5c543a1b9b1db9a8fafb22
Sacha Weatherstone <sachaw100@hotmail.com>
2024-03-25 16:55:54 +1000

    update deps, re-export utils

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from "./constants.js";
 export * from "./meshDevice.js";
 export * as Protobuf from "./protobufs.js";
 export * as Types from "./types.js";
+export * as Utils from "./utils/index.js";
