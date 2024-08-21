b94a3701e79ef745139817fa0a44bf94aa4f35a6
Ronin <6971304+TheCyberRonin@users.noreply.github.com>
2024-08-20 22:46:26 -0400

    chore: Add NodeJS Serial example

---
+++ example/nodeSerial/example.js
@@ -0,0 +1,56 @@
+import { NodeSerialConnection } from "./dist/index.js";
+import { SerialPort } from "serialport";
+
+const Connect = async () => {
+  const connection = new NodeSerialConnection();
+  await connection.connect({
+    portPath: 'COM4',
+    concurrentLogOutput: false
+  });
+  console.log(await SerialPort.list());
+  connection.events.onMessagePacket.subscribe((packet) => {
+    onMessage(packet.from, packet.data);
+  });
+  
+  connection.events.onPrivatePacket.subscribe((packet) => {
+    onMessage(packet.from, packet.data);
+  });
+  
+  connection.events.onLogEvent.subscribe((packet) => {
+    console.log('LogEvent: ', packet);
+  });
+
+  connection.events.onDeviceMetadataPacket.subscribe((packet) => {
+    console.log('DeviceMetadataPacket: ', packet);
+  })
+
+  connection.events.onDeviceDebugLog.subscribe((packet) => {
+    console.log('DeviceDebugLog: ', packet);
+  })
+
+  connection.events.onFromRadio.subscribe((packet) => {
+    console.log('FromRadio: ', packet);
+  });
+
+  connection.events.onDeviceStatus.subscribe((packet) => {
+    console.log('DeviceStatus: ', packet);
+  });
+
+  connection.events.onMyNodeInfo.subscribe((packet) => {
+    console.log('NodeInfo: ', packet);
+  })
+  const onMessage = (sender, message) => {
+  console.log("Message from: " + sender);
+  console.log("Message was: " + message);
+  };
+
+  connection.events.onRemoteHardwarePacket.subscribe((packet) => {
+    console.log('Remote Hardware Packet: ', packet);
+  });
+  
+  connection.events.onRoutingPacket.subscribe((packet) => {
+    console.log('Routing packet: ', packet);
+  });
+}
+
+Connect();
